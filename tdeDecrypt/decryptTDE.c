459e8cd1ffe34f33af6fe32249b475f3b2f8df2a
Aftersol <42478428+Aftersol@users.noreply.github.com>
2021-10-08 01:12:52 -0700

    Add files via upload

--- tdeDecrypt/decryptTDE.c
+++ tdeDecrypt/decryptTDE.c
@@ -1,4 +1,6 @@
+#if (_MSC_VER)
 #define _CRT_SECURE_NO_WARNINGS
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,11 +8,6 @@
 
 const char* const a = "599Cc51887A8cb0C20F9CdE34cf9e0A535E5cAd1C26c7b562596ACC207Ae9A0bfB3E3161f31af5bEf1c2f064b3628174D83BF6E0739D9D6918fD9C2Eba02D5aD\0";
 const char* const b = "0C3b676fe8d7188Cde022F71632830F36b98b181aD48Fed160006eA59\0";
-
-const char* input = "marina.ogg.tde";
-const char* output = "marina.ogg";
-//auto input = "D:\\Steam\\steamapps\\common\\Teardown\\data\\gfx\\foam.png.tde";
-//auto output = "D:\\Project\\TD\\test\\foam.png";
 
 int main()
 {
@@ -29,9 +26,6 @@ int main()
 
 	FILE* infile = fopen(inputName, "rb");
 	FILE* outfile = fopen(outputName, "wb");
-
-	/*FILE* infile = fopen(input, "rb");
-	FILE* outfile = fopen(output, "wb");*/
 
 	if (!infile)
 	{
